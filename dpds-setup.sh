3834b7c6e6ae99e4a4995d4a9b3f417dd5ae5a57
Anthony hopkins <anthony.b.hopkins@gmail.com>
2023-01-13 16:44:50 -0700

    Update dpds-setup.sh

--- dpds-setup.sh
+++ dpds-setup.sh
@@ -11,17 +11,11 @@
 # Both of the above arguments need to be unique and can not be the same as a built in Python module.
 
 # ARGS check for sanity.
-if [[ "$#" -ne 2 ]]
+if [[ "$#" -ne 1 ]]
 then
-  echo "This script requires two arguments - django project name and django app name."
-  echo "The django app name SHOULD be different than your Django project name to avoid confusion."
+  echo "This script requires one argument - django project name."
   echo "Exiting....."
   exit 1
-  if [[ "${1}" == "${2}" ]]
-  then
-    echo "Project name and App name are the same. Please use different names to avoid confusion."
-    exit 1
-  fi
 fi
 
 # Clean up curl installation and unzip clutter
