c7ab115bda1f8fd156be29e6bfa3a9142d025d7a
Anthony hopkins <anthony.b.hopkins@gmail.com>
2023-01-13 13:15:13 -0700

    Update build_baseline.sh

--- build_baseline.sh
+++ build_baseline.sh
@@ -4,8 +4,10 @@
 # ensure that the <APP> instances are all replaced with the application name via user provided parameter.
 
 # Check number of arguments and exit if incorrect
-if [[ "$#" -ne 1 ]]; then
-  echo "Invalid number of arguments. Only provide the name of the Django app."
+if [[ "$#" -ne 0 ]]; then
+  echo "Please run without providing arguments - they are not necessary."
   else
-    sed -i "s/<APP>/${1}/g" ./{Dockerfile,docker-compose.yml}
+    # Assign current base directory to a variable to pass to sed for replacement purposes.
+    APPDIR=$(basename $(pwd))
+    sed -i "s/<APP>/${APPDIR}/g" ./{Dockerfile,docker-compose.yml}
 fi
