8a7d8432f5116fa002792e3b792e128772f773cb
Anthony hopkins <anthony.b.hopkins@gmail.com>
2023-01-13 14:01:19 -0700

    Update build_baseline.sh

--- build_baseline.sh
+++ build_baseline.sh
@@ -10,4 +10,8 @@ if [[ "$#" -ne 0 ]]; then
     # Assign current base directory to a variable to pass to sed for replacement purposes.
     APPDIR=$(basename $(pwd))
     sed -i "s/<APP>/${APPDIR}/g" ./{Dockerfile,docker-compose.yml}
+    # Move the management directory to the current application directory to allow the use of the custom commands.
+    mv ./management ./${APPDIR}/
+    # Clean up the artifacts from the download and unzip process ran prior to this script.
+    rm -rf ./{django_postgresql_docker_start-main,main.zip,README.md}
 fi
