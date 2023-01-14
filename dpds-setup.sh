970358009022cb5ec7680bebf40ae89d0fe28f38
Anthony hopkins <anthony.b.hopkins@gmail.com>
2023-01-13 17:34:06 -0700

    Update dpds-setup.sh

--- dpds-setup.sh
+++ dpds-setup.sh
@@ -33,7 +33,7 @@ django-admin startproject ${PROJ_NAME}
 mv ./core ./${PROJ_NAME}/
 
 # Replace Django app's settings.py file with a osycopg/wait_for_db ready version.
-rm ./${PROJ_NAME}/settings.py && mv ./settings.py ./${PROJ_NAME}
+rm ./${PROJ_NAME}/${PROJ_NAME}/settings.py && mv ./settings.py ./${PROJ_NAME}/${PROJ_NAME}
 
 # Use sed to replace <APP> with out Django project name to ensure our Docker files are properly configured. 
 sed -i "s/<APP>/${PROJ_NAME}/g" ./{Dockerfile,docker-compose.yml}
